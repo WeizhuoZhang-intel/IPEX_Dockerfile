b936cbe98be0b3dfc3bccff2c14a36200e5360c5
mengfeil <mengfei.li@intel.com>
2023-05-19 23:29:13 +0800

    add tpp script

--- run_gptj.py
+++ run_gptj.py
@@ -1,17 +1,12 @@
+import transformers
 from transformers import GPTJForCausalLM, AutoTokenizer
 import torch
 import time
 import json
 import pathlib
-import psutil
 import argparse
 import numpy as np
 from itertools import chain
-
-def trace_handler(prof):
-    print(prof.key_averages().table(
-        sort_by="self_cpu_time_total", row_limit=-1))
-    prof.export_chrome_trace("my_trace.log" + str(prof.step_num) + ".json")
 
 # args
 parser = argparse.ArgumentParser("GPT-J generation script", add_help=False)
@@ -25,17 +20,9 @@ parser.add_argument(
     default="EleutherAI/gpt-j-6B",
 )
 parser.add_argument(
-    "--device",
-    type=str,
-    choices=["cpu", "xpu", "cuda", "hpu"],
-    help="cpu, xpu, hpu or cuda",
-    default="cpu",
-)
-parser.add_argument(
     "--dtype",
     type=str,
-    choices=["float32", "bfloat16", "float16"],
-    help="bfloat16 or float32 or float16",
+    help="bfloat16 or float32",
     default="bfloat16",
 )
 parser.add_argument(
@@ -49,45 +36,38 @@ parser.add_argument("--prompt", default=None, type=str)
 parser.add_argument("--num-iter", default=100, type=int, help="num iter")
 parser.add_argument("--num-warmup", default=10, type=int, help="num warmup")
 parser.add_argument("--batch-size", default=1, type=int, help="batch size")
-parser.add_argument("--print-memory", action="store_true")
 parser.add_argument("--token-latency", action="store_true")
+parser.add_argument("--use-tpp", action="store_true")
 args = parser.parse_args()
 print(args)
 
 
-def get_memory_usage(name, args):
-    if args.print_memory:
-        if args.device == "cuda":
-            memory_allocated = round(torch.cuda.memory_reserved() / 1024**3, 3)
-        elif args.device == "hpu":
-            memory_allocated = round(ht.hpu.memory_allocated(0) / 1024**3, 3)
-        elif args.device == "xpu":
-            memory_allocated = round(torch.xpu.memory_reserved() / 1024**3, 3)
-        else:
-            memory_allocated = round(psutil.Process().memory_info().rss / 1024**3, 3)
-
-        print(name, "memory used total:", memory_allocated, "GB")
-
-
 # device
-device = torch.device(args.device)
+device = torch.device('cpu')
 # import extension
 if args.ipex:
     import intel_extension_for_pytorch as ipex
-
-if args.device == "hpu":
-    import habana_frameworks.torch as ht
 
 # dtype
 if args.dtype == "bfloat16":
     amp_enabled = True
     amp_dtype = torch.bfloat16
-elif args.dtype == "float16":
-    amp_enabled = True
-    amp_dtype = torch.float16
 else:
     amp_enabled = False
     amp_dtype = torch.float32
+
+# generate args
+if args.greedy:
+    generate_kwargs = dict(do_sample=False, temperature=0.9)
+else:
+    generate_kwargs = dict(do_sample=False, temperature=0.9, num_beams=4)
+if args.jit:
+    torch._C._jit_set_texpr_fuser_enabled(False)
+    generate_kwargs["jit"] = True
+if args.token_latency:
+    generate_kwargs["token_latency"] = True
+if args.use_tpp:
+    generate_kwargs["use_tpp"] = True
 
 # load model
 model_id = args.model_id
@@ -95,19 +75,21 @@ model = GPTJForCausalLM.from_pretrained(
     model_id, low_cpu_mem_usage=True, return_dict=not args.jit, torch_dtype=amp_dtype
 )
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-get_memory_usage("Host", args)
 model = model.eval().to(device)
-get_memory_usage("Device", args)
 model = model.to(memory_format=torch.channels_last)
 
-# to hpu graph
-if args.device == "hpu":
-    model = ht.hpu.wrap_in_hpu_graph(model)
-    get_memory_usage("Graph", args)
 # to ipex
 if args.ipex:
     model = ipex.optimize(model.eval(), dtype=amp_dtype, inplace=True)
-    get_memory_usage("Ipex", args)
+
+# use tpp
+if args.use_tpp:
+    from tpp_pytorch_extension.llm.fused_gptj_infer import FixGPTJBlock, block
+    for m in model.modules():
+        if isinstance(m, transformers.models.gptj.modeling_gptj.GPTJBlock):
+            # FixGPTJBlock(m, 16, 16, torch.bfloat16)
+            FixGPTJBlock(m, 16, 64, amp_dtype)
+    # block(model)
 
 # input prompt
 current_path = pathlib.Path(__file__).parent.resolve()
@@ -123,17 +105,6 @@ else:
 input_size = tokenizer(prompt, return_tensors="pt").input_ids.size(dim=1)
 print("---- Prompt size:", input_size)
 
-# generate args
-if args.greedy:
-    generate_kwargs = dict(do_sample=False, temperature=0.9)
-else:
-    generate_kwargs = dict(do_sample=False, temperature=0.9, num_beams=4)
-if args.jit:
-    torch._C._jit_set_texpr_fuser_enabled(False)
-    generate_kwargs["jit"] = True
-if args.token_latency:
-    generate_kwargs["token_latency"] = True
-
 # start
 total_time = 0.0
 num_iter = args.num_iter
@@ -146,7 +117,6 @@ with torch.inference_mode(), torch.no_grad(), torch.autocast(
     dtype=amp_dtype if amp_enabled else None,
 ):
     for i in range(num_iter):
-        get_memory_usage("Iteration: " + str(i), args)
         tic = time.time()
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
         output = model.generate(
@@ -154,12 +124,6 @@ with torch.inference_mode(), torch.no_grad(), torch.autocast(
         )
         gen_ids = output[0] if args.token_latency else output
         gen_text = tokenizer.batch_decode(gen_ids, skip_special_tokens=True)
-        if args.device == "xpu":
-            torch.xpu.synchronize()
-        elif args.device == "cuda":
-            torch.cuda.synchronize()
-        elif args.device == "hpu":
-            gen_ids.to("cpu")
         toc = time.time()
         print(gen_text, flush=True)
         if i >= num_warmup:
@@ -176,6 +140,7 @@ if args.token_latency:
     average_2n.sort()
     average_2n_latency = np.mean(average_2n)
     p90_latency = average_2n[int(len(average_2n) * 0.9)]
+    p99_latency = average_2n[int(len(average_2n) * 0.99)]
     print("First token average latency: %.3f sec." % first_latency)
     print("Average 2... latency: %.3f sec." % average_2n_latency)
-    print("P90 2... latency: %.3f sec." % p90_latency)
+    print("P99 2... latency: %.3f sec." % p90_latency)
