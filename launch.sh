58dc738c8ddc3934db869579d7ba912512ab7326
ESI-SYD <yudong.si@intel.com>
2023-07-05 11:08:52 +0800

    upd launch

--- launch.sh
+++ launch.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # set -x
 
-kmp=${1:-''}
-log_dir=${2:-log_dir}
+ds=${1:-disable}
+kmp=${2:-''}
+log_dir=${3:-log_dir}
 rm -rf ${log_dir}
 mkdir -p $log_dir
 
 # collec result
 function run_benchmark {
     # generate cmd
-    python generate_run_script.py ${kmp}
+    if [[ $ds == "enable" ]]; then
+        echo "Enable DeepSpeed script generation"
+        python generate_run_script.py ${kmp} --deepspeed
+    else
+        python generate_run_script.py ${kmp}
+    fi
     # run benchmark
     for item in `ls | grep run_ | grep .sh`; do
         bash ${item}
