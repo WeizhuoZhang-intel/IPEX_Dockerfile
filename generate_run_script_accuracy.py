0c6f1248b20f34c093f51644af60f747c29b1d07
MingxuZh <109504044+MingxuZh@users.noreply.github.com>
2023-08-09 12:31:01 +0800

    Update generate_run_script_accuracy.py

--- generate_run_script_accuracy.py
+++ generate_run_script_accuracy.py
@@ -124,7 +124,8 @@ def generate_commands(yml_file,mode,extra_kmp):
         lines.append(f"export KMP_FORJOIN_BARRIER_PATTERN={data['envconfig']['LLM_EXTRA_KMP']['KMP_FORJOIN_BARRIER_PATTERN']}")
         lines.append(f"export KMP_PLAIN_BARRIER_PATTERN={data['envconfig']['LLM_EXTRA_KMP']['KMP_PLAIN_BARRIER_PATTERN']}")
         lines.append(f"export KMP_REDUCTION_BARRIER_PATTERN={data['envconfig']['LLM_EXTRA_KMP']['KMP_REDUCTION_BARRIER_PATTERN']}")
-
+        lines.append("export TRANSFORMERS_OFFLINE=1")
+        
         lines.append("log_dir=${1:-log_dir}")
         lines.append("mkdir -p $log_dir")
         lines.append("# device info")
