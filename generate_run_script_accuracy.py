714caf3064751b22a6f9a6297b159a8a0bb7c6b7
MingxuZh <109504044+MingxuZh@users.noreply.github.com>
2023-08-15 21:48:22 +0800

    Update generate_run_script_accuracy.py

--- generate_run_script_accuracy.py
+++ generate_run_script_accuracy.py
@@ -201,7 +201,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                     lines.append(f"deepspeed --bind_cores_to_rank run_generation_with_deepspeed.py -m {data['modelargs'][mode]['shardpath']} --output-dir {data['modelargs'][mode]['outdir']} --dtype float32 --ipex --jit --ipex-weight-only-quantization")
                     # lines.append(f"python run_generation_with_deepspeed.py --ipex-smooth-quant --lambada --output-dir {data['modelargs'][mode]['outdir']} --jit --int8 -m {model_id}")
 
-                    lines.append(f"deepspeed  --num_gpus 2 --master_addr `hostname -I | sed -e 's/\s.*$//'` --bind_cores_to_rank run_accuracy_with_deepspeed.py --device cpu --model {data['modelargs'][mode]['shardpath']} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype bfloat16 --profile --ipex --jit --tasks lambada_openai --accuracy-only \
+                    lines.append(f"deepspeed  --num_gpus 2 --master_addr `hostname -I | sed -e 's/\s.*$//'` --bind_cores_to_rank run_accuracy_with_deepspeed.py --device cpu --model {data['modelargs'][mode]['shardpath']} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype float32 --profile --ipex --jit --tasks lambada_openai --accuracy-only \
                                 2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_dswoq_{dtype}_accuracy.log")
         
         lines.append(f"sleep 5s")
