5c4b7dad32b5006ac4244cbb2d4a269ea4b1f96b
MingxuZh <109504044+MingxuZh@users.noreply.github.com>
2023-08-18 12:15:57 +0800

    Update generate_run_script.py

--- generate_run_script.py
+++ generate_run_script.py
@@ -183,7 +183,16 @@ def generate_commands(yml_file,mode,extra_kmp):
             #             else:
             #                 lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -N {data['launcher']['numactlN']} -m {data['launcher']['numactlM']} python {data['modelargs'][mode]['scriptname']} --quantized_model_path \
             #                 {data['modelargs'][mode]['quantizedmodelpath']} --input-tokens {input_token} --benchmark --jit --int8 -m {model_id} --token-latency 2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_int8_{input_token}_greedy_{beam}.log")
-                            lines.append(f"collect_perf_logs_llm llm_default_{model_id.replace('/','-')}_int8_{input_token}-{output}_greedy_{beam}.log")        
+                            lines.append(f"collect_perf_logs_llm llm_default_{model_id.replace('/','-')}_int8_{input_token}-{output}_greedy_{beam}.log") 
+        if mode.endswith('bf16'):
+            for model_id in data['modelargs'][mode]['modelid']:
+                # lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-weight-only-quantization --lambada --output-dir {data['modelargs'][mode]['outputdir']} --jit --int8-bf16-mixed --lowp-mode 'BF16' -m {model_id}")
+                for input_token in data['modelargs'][mode]['inputtokens']:
+                    for output in data['modelargs'][mode]['maxnewtokens']:
+                        for beam in data['modelargs'][mode]['greedy']:
+                            lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -N {data['launcher']['numactlN']} -m {data['launcher']['numactlM']} python {data['modelargs'][mode]['scriptname']} --device cpu --input-tokens {input_token} --max-new-tokens {output} --jit -m {model_id} --dtype bfloat16 --lambada --benchmark --token-latency \
+                             2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_bf16_{input_token}-{output}_greedy_{beam}.log")
+        
         if mode == 'deepspeed':
             lines.append("# DS Env config")
             lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
