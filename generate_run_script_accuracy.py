74e6aa50749dc477a47fcc401be6800d1fdca158
mingxu <mingxu.zhang@intel.com>
2023-08-24 09:40:57 +0800

    acc

--- generate_run_script_accuracy.py
+++ generate_run_script_accuracy.py
@@ -203,7 +203,7 @@ def generate_commands(yml_file,mode,extra_kmp):
 
                     # lines.append(f"deepspeed  --num_gpus 2 --master_addr `hostname -I | sed -e 's/\s.*$//'` --bind_cores_to_rank run_accuracy_with_deepspeed.py --device cpu --model {data['modelargs'][mode]['shardpath']} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype float32 --profile --ipex --jit --tasks lambada_openai --accuracy-only \
                     #             2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_dswoq_{dtype}_accuracy.log")
-                    lines.append(f"deepspeed  --num_gpus 2 --master_addr `hostname -I | sed -e 's/\s.*$//'` --bind_cores_to_rank run_accuracy_with_deepspeed.py --device cpu --model {data['modelargs'][mode]['shardpath']} --dtype bfloat16 --ipex --jit --tasks lambada_openai --accuracy-only \
+                    lines.append(f"deepspeed  --num_gpus 2 --master_addr `hostname -I | sed -e 's/\s.*$//'` --bind_cores_to_rank run_accuracy_with_deepspeed.py --device cpu --model {model_id} --dtype bfloat16 --ipex --jit --tasks lambada_openai --accuracy-only \
                                 2>&1 | tee -a $log_dir/llm_deepspeed_{model_id.replace('/','-')}_{dtype}_accuracy.log")
         
         lines.append(f"sleep 5s")
