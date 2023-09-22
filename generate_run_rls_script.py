8bfdd58d58792bceb89fad768697b6984d42e094
mingxu <mingxu.zhang@intel.com>
2023-09-22 14:25:56 +0800

    debug

--- generate_run_rls_script.py
+++ generate_run_rls_script.py
@@ -457,6 +457,27 @@ def generate_commands(yml_file,mode,extra_kmp):
                                             lines.append(f"collect_perf_logs_llm llm_deepspeed_{model_id.replace('/','-')}_woqbf16mixed_{input_token}-{output_token}_greedy_False_NUMA_{numa}_INT4.log")                                        
 
 
+
+        if mode.endswith('gptq'):
+            lines.append("# Run Workload")
+            for model_id in data['modelargs'][mode]['modelid']:
+                lines.append(f"rm -rf {data['modelargs'][mode]['outputdir']}")
+                lines.append(f"mkdir -p {data['modelargs'][mode]['outputdir']}")
+                lines.append(f"python run_gptq.py --model {model_id} --out-dir {data['modelargs'][mode]['outputdir']}")
+
+        # if mode.endswith('gptqint4'):
+            lines.append("# Run Workload")       
+            for model_id in data['modelargs'][mode]['modelid']:
+                if model_id == "EleutherAI/gpt-neox-20b":
+                    lines.append(f"python {data['modelargs'][mode]['scriptnamerun']} --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outputdir']} --int8 --weight-dtype INT8 --lowp-mode BF16 -m {model_id}")
+                elif model_id == "tiiuae/falcon-40b":
+                    lines.append(f"python run_falcon_quantization.py --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outputdir']} --int8-bf16-mixed -m <MODEL_ID> --config-file /root/workspace/IPEX_Dockerfile/tiiuae_falcon-40b_config.json")
+                else:
+                    lines.append(f"python {data['modelargs'][mode]['scriptnamerun']} --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outputdir']} --int8-bf16-mixed --low-precision-checkpoint {data['modelargs'][mode]['outputpt']} -m {model_id}")
+
+                lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -N {data['launcher']['numactlN']} -m {data['launcher']['numactlM']} python {data['modelargs'][mode]['scriptnamerun']} -m {model_id} --quantized-model-path {data['modelargs'][mode]['quantizedmodelpath']} --benchmark --int8-bf16-mixed --token-latency")
+
+ 
         # if mode == "default":
         #     lines.append("# Run workload")
         #     for model_id in data['modelargs'][mode]['modelid']:
