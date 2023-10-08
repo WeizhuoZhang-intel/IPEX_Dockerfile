5b8d8603a16b5696795b79a4926cffcc6ecf30b4
mingxu <mingxu.zhang@intel.com>
2023-10-08 12:58:49 +0800

    debug

--- generate_run_script_acc_rls.py
+++ generate_run_script_acc_rls.py
@@ -185,9 +185,9 @@ def generate_commands(yml_file,mode,extra_kmp):
                         lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype int8 --jit --tasks lambada_openai \
                                     2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_woqint8_{dtype}_accuracy.log")
                     else:
-                        lines.append(f"rm -rf {data['modelargs'][mode]['outdir']}")
-                        lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
-                        lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-smooth-quant --output-dir {data['modelargs'][mode]['outdir']} --int8-bf16-mixed -m {model_id}")
+                        # lines.append(f"rm -rf {data['modelargs'][mode]['outdir']}")
+                        # lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
+                        # lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-smooth-quant --output-dir {data['modelargs'][mode]['outdir']} --int8-bf16-mixed -m {model_id}")
                         lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
                         lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype {dtype} --int8-bf16-mixed --jit --tasks lambada_openai \
                                     2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_bf16mixed_{dtype}_accuracy.log")
