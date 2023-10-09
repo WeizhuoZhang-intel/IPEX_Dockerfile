f893bcf81b9276d3a93e17f54ea0c1d476e86286
mingxu <mingxu.zhang@intel.com>
2023-10-09 21:58:33 +0800

    debug

--- generate_run_rls_script.py
+++ generate_run_rls_script.py
@@ -300,8 +300,12 @@ def generate_commands(yml_file,mode,extra_kmp):
                 for dtype in data['modelargs'][mode]['dtype']:
                     for input_token in data['modelargs'][mode]['inputtokens']:
                         for output_token in data['modelargs'][mode]['maxnewtokens']:
+                            lines.append(f"export local_rank=2")
+                            lines.append("deepspeed_core_config ${local_rank}")
+                            lines.append("export CCL_WORKER_AFFINITY=${deepspeed_cores_list}")
+                            lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
                             lines.append(f"nohup bash /root/workspace/get_mem.sh >> $log_dir/mem-usage-llm_deepspeed_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}_greedy_False_NUMA_2_BF16.log 2>&1 || true &")
-                            lines.append(f"deepspeed --bind_cores_to_rank run.py --benchmark -m {model_id} --dtype {dtype} --input-tokens {input_token} \
+                            lines.append(f"deepspeed --bind_cores_to_rank --num_accelerators 2 --bind_core_list $core_list run.py --benchmark -m {model_id} --dtype {dtype} --input-tokens {input_token} \
                                         --max-new-tokens {output_token} --ipex --deployment-mode --token-latency --num-iter 50 --autotp --shard-model 2>&1 | tee -a $log_dir/llm_deepspeed_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}_greedy_False_NUMA_2_BF16.log") 
                             lines.append(f"collect_perf_logs_llm llm_deepspeed_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}_greedy_False_NUMA_2_BF16.log")
 
@@ -315,9 +319,13 @@ def generate_commands(yml_file,mode,extra_kmp):
                 for dtype in data['modelargs'][mode]['dtype']:
                     for input_token in data['modelargs'][mode]['inputtokens']:
                         for output_token in data['modelargs'][mode]['maxnewtokens']:
+                            lines.append(f"export local_rank=2")
+                            lines.append("deepspeed_core_config ${local_rank}")
+                            lines.append("export CCL_WORKER_AFFINITY=${deepspeed_cores_list}")
+                            lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
                             lines.append(f"nohup bash /root/workspace/get_mem.sh >> $log_dir/mem-usage-llm_deepspeed_{model_id.replace('/','-')}_woq-int8_{input_token}-{output_token}_greedy_False_NUMA_2_BF16.log 2>&1 || true &")
                             if 'neox' in model_id:
-                                lines.append(f"deepspeed --bind_cores_to_rank run.py --benchmark -m {model_id} --int8 --input-tokens {input_token} \
+                                lines.append(f"deepspeed --bind_cores_to_rank --num_accelerators 2 --bind_core_list $core_list run.py --benchmark -m {model_id} --int8 --input-tokens {input_token} \
                                             --max-new-tokens {output_token} --ipex --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outputdir']} --deployment-mode --token-latency --num-iter 50 --autotp --shard-model 2>&1 | tee -a $log_dir/llm_deepspeed_{model_id.replace('/','-')}_woq-int8_{input_token}-{output_token}_greedy_False_NUMA_2_BF16.log") 
                             elif 'falcon' in model_id:
                                 lines.append(f"deepspeed --bind_cores_to_rank run.py --benchmark -m {model_id} --int8-bf16-mixed --input-tokens {input_token} \
