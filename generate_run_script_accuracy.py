b9770973e7471df27be84e9f30c76f1e5e0c2cef
mingxu <mingxu.zhang@intel.com>
2023-10-23 11:42:51 +0800

    acc

--- generate_run_script_accuracy.py
+++ generate_run_script_accuracy.py
@@ -189,7 +189,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                         #             2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_bf16mixed_{dtype}_accuracy.log")
                         lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
                         lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
-                        lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} --ipex-smooth-quant --lambada --output-dir {data['modelargs'][mode]['outdir']} --jit --int8-bf16-mixed -m {model_id} --lowp-mode 'BF16' --accuracy-only --dtype {dtype} --tasks lambada_openai \
+                        lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} --jit --int8-bf16-mixed -m {model_id} --accuracy-only --dtype {dtype} --tasks lambada_openai \
                                      2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_static-int8_{dtype}_accuracy.log")
                         
                         # lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype {dtype} --int8-bf16-mixed --jit --tasks lambada_openai \
