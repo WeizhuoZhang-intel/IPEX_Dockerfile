d67efb7a8fbb340aca70640752675f93446a0198
mingxu <mingxu.zhang@intel.com>
2023-11-02 14:34:01 +0800

    debug

--- generate_run_script.py
+++ generate_run_script.py
@@ -415,7 +415,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                                     lines.append(f"deepspeed --bind_cores_to_rank --num_accelerators {numa} --bind_core_list $core_list {data['modelargs'][mode]['scriptname']} --device {data['modelargs'][mode]['device'][0]} --benchmark -m {model_id} --dtype {dtype} --input-tokens {input_token} \
                                             --max-new-tokens {output_token} --ipex --jit --token-latency --num-iter 50 2>&1 | tee -a $log_dir/llm_deepspeed_{model_id.replace('/','-')}_ds-{dtype}_{input_token}-{output_token}_greedy_False_NUMA_{numa}_BF16.log") 
                                 else:
-                                    lines.append(f"deepspeed --bind_cores_to_rank --num_accelerators {numa} --bind_core_list $core_list {data['modelargs'][mode]['scriptname']} --device {data['modelargs'][mode]['device'][0]} --benchmark -m {data['modelargs'][mode]['shardpath']} --dtype {dtype} --input-tokens {input_token} \
+                                    lines.append(f"deepspeed --bind_cores_to_rank --num_accelerators {numa} --bind_core_list $core_list {data['modelargs'][mode]['scriptname']} --device {data['modelargs'][mode]['device'][0]} --benchmark -m {model_id} --dtype {dtype} --input-tokens {input_token} \
                                             --max-new-tokens {output_token} --ipex --jit --token-latency --num-iter 50 2>&1 | tee -a $log_dir/llm_deepspeed_{model_id.replace('/','-')}_ds-{dtype}_{input_token}-{output_token}_greedy_False_NUMA_{numa}_BF16.log") 
                                 lines.append(f"collect_perf_logs_llm llm_deepspeed_{model_id.replace('/','-')}_ds-{dtype}_{input_token}-{output_token}_greedy_False_NUMA_{numa}_BF16.log")
         if mode.endswith('2s1'):
