bf839c76e7775a781f6f0495accb8da027c3d4a7
mingxu <mingxu.zhang@intel.com>
2023-11-15 10:13:07 +0800

    acc

--- generate_run_rls_script.py
+++ generate_run_rls_script.py
@@ -935,6 +935,87 @@ def generate_commands(yml_file,mode,extra_kmp):
                                                 2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_fp32-int8_{input_token}-{output_token}_greedy_{beam}_NUMA_1_BF16.log")                                    
                                 lines.append(f"collect_perf_logs_llm llm_default_{model_id.replace('/','-')}_fp32-int8_{input_token}-{output_token}_greedy_{beam}_NUMA_1_BF16.log")
 
+
+        if mode.endswith('int8acc'):
+            lines.append("# DS Env config")
+            lines.append("unset KMP_AFFINITY")
+            # lines.append("export TRANSFORMERS_OFFLINE=0")
+            # lines.append("pip install --upgrade huggingface_hub")
+            # lines.append("huggingface-cli login --token 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")   
+            lines.append("# Run workload")  
+            lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")  
+            for model_id in data['modelargs'][mode]['modelid']:
+                for dtype in data['modelargs'][mode]['dtype']:
+
+                    if model_id == "EleutherAI/gpt-neox-20b":
+                        lines.append(f"rm -rf {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outdir']} --int8 -m {model_id} --dataset NeelNanda/pile-10k")
+                        lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
+                        lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype int8 --jit --tasks lambada_openai \
+                                    2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_woqint8_{dtype}_accuracy.log")
+                    else:
+                        lines.append(f"rm -rf {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-smooth-quant --output-dir {data['modelargs'][mode]['outdir']} --int8-bf16-mixed -m {model_id} --dataset NeelNanda/pile-10k --alpha {data['modelargs'][mode]['alpha']}")
+                        lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
+                        lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype {dtype} --int8-bf16-mixed --jit --tasks lambada_openai \
+                                    2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_static-int8_{dtype}_accuracy.log")
+
+        if mode.endswith('int8accfp32'):
+            lines.append("# DS Env config")
+            lines.append("unset KMP_AFFINITY")
+            # lines.append("export TRANSFORMERS_OFFLINE=0")
+            # lines.append("pip install --upgrade huggingface_hub")
+            # lines.append("huggingface-cli login --token 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")   
+            lines.append("# Run workload")  
+            lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")  
+            for model_id in data['modelargs'][mode]['modelid']:
+                for dtype in data['modelargs'][mode]['dtype']:
+
+                    if model_id == "EleutherAI/gpt-neox-20b":
+                        lines.append(f"rm -rf {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outdir']} --int8 -m {model_id} --dataset NeelNanda/pile-10k")
+                        lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
+                        lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype int8 --jit --tasks lambada_openai \
+                                    2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_woqint8_{dtype}_accuracy.log")
+                    else:
+                        lines.append(f"rm -rf {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"mkdir -p {data['modelargs'][mode]['outdir']}")
+                        lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-smooth-quant --output-dir {data['modelargs'][mode]['outdir']} --int8-bf16-mixed -m {model_id} --dataset NeelNanda/pile-10k --alpha {data['modelargs'][mode]['alpha']}")
+                        lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
+                        lines.append(f"numactl -m 0 -N 0 python run_accuracy.py --accuracy-only -m {model_id} --quantized-model-path {data['modelargs'][mode]['bestpath']} --dtype {dtype} --int8-bf16-mixed --jit --tasks lambada_openai \
+                                    2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_static-int8_{dtype}_accuracy.log")
+
+        if mode == 'defaultacc':
+            lines.append("# DS Env config")
+
+            lines.append("unset KMP_AFFINITY")
+            # lines.append("export TRANSFORMERS_OFFLINE=0")
+            # lines.append("pip install --upgrade huggingface_hub")
+            # lines.append("huggingface-cli login --token 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")   
+            lines.append("# Run workload")
+            lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
+            for model_id in data['modelargs'][mode]['modelid']:
+                for dtype in data['modelargs'][mode]['dtype']:
+                    lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} --accuracy-only -m {model_id} --dtype {dtype} --ipex --jit --tasks lambada_openai \
+                                 2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_{dtype}_accuracy.log")
+        if mode == 'bf16acc':
+            lines.append("# DS Env config")
+
+            lines.append("unset KMP_AFFINITY")
+            # lines.append("export TRANSFORMERS_OFFLINE=0")
+            # lines.append("pip install --upgrade huggingface_hub")
+            # lines.append("huggingface-cli login --token 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")   
+            lines.append("# Run workload")
+            lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
+            for model_id in data['modelargs'][mode]['modelid']:
+                for dtype in data['modelargs'][mode]['dtype']:
+                    lines.append(f"numactl -m 0 -N 0 python {data['modelargs'][mode]['scriptname']} --accuracy-only -m {model_id} --dtype {dtype} --tasks lambada_openai \
+                                 2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','-')}_{dtype}_accuracy.log")
+
+
         if mode.endswith('2s'):
             lines.append("# Run Workload")
 
