0499c3eb81219fd9631b6170f712ebba9b140aa7
mingxu <mingxu.zhang@intel.com>
2023-11-22 23:51:01 +0800

    debug

--- generate_run_rls_docker_script.py
+++ generate_run_rls_docker_script.py
@@ -497,7 +497,7 @@ def generate_commands(yml_file,mode,extra_kmp):
             lines.append("export WORK_DIR=./")
             for model_id in data['modelargs'][mode]['modelid']:
                 lines.append(f"mkdir -p {data['modelargs'][mode]['outputdir']}")
-                lines.append(f"python ./utils/run_gptq.py --model {model_id} --output-dir {data['modelargs'][mode]['outputdir']}")
+                lines.append(f"python utils/run_gptq.py --model {model_id} --output-dir {data['modelargs'][mode]['outputdir']}")
                 lines.append("wait")
                 lines.append(f"python {data['modelargs'][mode]['scriptname']} --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outputdir']} --int8-bf16-mixed -m {model_id} --low-precision-checkpoint {data['modelargs'][mode]['gptqpath']}")
 
@@ -507,7 +507,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                 lines.append(f"export local_rank={rank}")
                 lines.append("deepspeed_core_config ${local_rank}")
                 lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
-                lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m 0 -C $core_list python ./single_instance/run_accuracy.py --accuracy-only -m {model_id} --dtype bfloat16 --ipex --jit --tasks lambada_openai \
+                lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m 0 -C $core_list python single_instance/run_accuracy.py --accuracy-only -m {model_id} --dtype bfloat16 --ipex --jit --tasks lambada_openai \
                              2>&1 | tee -a $log_dir/llm_accuracy_{model_id.replace('/','-')}_bfloat16_{data['launcher']['hw']}.log")
                 
         if mode.endswith('woq8acc'):
@@ -515,7 +515,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                 lines.append(f"export local_rank={rank}")
                 lines.append("deepspeed_core_config ${local_rank}")
                 lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
-                lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m 0 -C $core_list python ./single_instance/run_accuracy.py --quantized-model-path {data['modelargs'][mode]['quantizedmodelpath']} --accuracy-only -m {model_id} --dtype bfloat16 --ipex --jit --tasks lambada_openai \
+                lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m 0 -C $core_list python single_instance/run_accuracy.py --quantized-model-path {data['modelargs'][mode]['quantizedmodelpath']} --accuracy-only -m {model_id} --dtype bfloat16 --ipex --jit --tasks lambada_openai \
                              2>&1 | tee -a $log_dir/llm_accuracy_{model_id.replace('/','-')}_bfloat16_{data['launcher']['hw']}.log")
                                             
 
