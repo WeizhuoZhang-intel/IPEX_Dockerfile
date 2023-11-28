278350302149caf8d36cba9c6f1fae5c55a59c23
mingxu <mingxu.zhang@intel.com>
2023-11-28 21:03:15 +0800

    debug

---
+++ generate_run_tgi.py
@@ -0,0 +1,123 @@
+# Generate runnable scripts for benchmarking
+import argparse
+import yaml
+parser = argparse.ArgumentParser("Generation script", add_help=False)
+parser.add_argument("--debug",action="store_true",default=False,help="only for debug regular track")
+
+
+
+collect_result = '''
+function collect_perf_logs_llm() {
+    # latency
+    sleep 5s
+    latency=($(grep -i 'inference latency:' $log_dir/$1 |sed -e 's/.*atency://;s/[^0-9.]//g;s/\.$//' |awk '
+        BEGIN {
+            num = 0;
+            sum = 0;
+        }{
+            num ++;
+            sum += $1;
+        }END {
+            if(num > 0) {
+                printf("%d  %.6f", num, sum / num);
+            }else {
+                printf("0  0");
+            }
+        }
+    '))
+    first_latency=($(grep -i 'First token average latency:' $log_dir/$1 |sed -e 's/.*atency://;s/[^0-9.]//g;s/\.$//' |awk '
+        BEGIN {
+            num = 0;
+            sum = 0;
+        }{
+            num ++;
+            sum += $1;
+        }END {
+            if(num > 0) {
+                printf("%.6f", sum / num);
+            }else {
+                printf("0");
+            }
+        }
+    '))
+    avg_latency=($(grep -i 'Average 2... latency:' $log_dir/$1 |sed -e 's/.*atency://;s/[^0-9.]//g;s/\.$//' |awk '
+        BEGIN {
+            num = 0;
+            sum = 0;
+        }{
+            num ++;
+            sum += $1;
+        }END {
+            if(num > 0) {
+                printf("%.6f", sum / num);
+            }else {
+                printf("0");
+            }
+        }
+    '))
+    p90_latency=($(grep -i 'P90 2... latency:' $log_dir/$1 |sed -e 's/.*atency://;s/[^0-9.]//g;s/\.$//' |awk '
+        BEGIN {
+            num = 0;
+            sum = 0;
+        }{
+            num ++;
+            sum += $1;
+        }END {
+            if(num > 0) {
+                printf("%.6f", sum / num);
+            }else {
+                printf("0");
+            }
+        }
+    '))
+    p99_latency=($(grep -i 'P99 2... latency:' $log_dir/$1 |sed -e 's/.*atency://;s/[^0-9.]//g;s/\.$//' |awk '
+        BEGIN {
+            num = 0;
+            sum = 0;
+        }{
+            num ++;
+            sum += $1;
+        }END {
+            if(num > 0) {
+                printf("%.6f", sum / num);
+            }else {
+                printf("0");
+            }
+        }
+    '))
+ 
+    peak_memory=$(grep '^Total' ${log_dir}/mem-usage-$1 |sed 's/[^0-9. ]//g' |awk 'BEGIN{peak=0}{if($NF > peak){peak = $NF}}END{print peak / 1024}') || peak_memory=0
+    printf $1 |tee -a ${log_dir}/summary.log
+    printf ", ${latency[1]},${first_latency},${avg_latency},${p90_latency},${p99_latency},${peak_memory} \\n" |tee -a ${log_dir}/summary.log
+}
+'''
+
+def generate_commands(yml_file,mode,extra_kmp):
+    data = yaml.load(open(yml_file, 'r'),Loader=yaml.FullLoader)
+    generated_file = "run_"+mode+".sh"
+    with open(generated_file, "w") as runfile:
+        lines = []
+        lines.append("#!/bin/bash")
+        lines.append("set -x")
+        lines.append("# Env config")
+        lines.append(f"export repopath={data['envconfig']['REPOPATH']}")
+        lines.append("while true")
+        lines.append("do")
+        lines.append(f"")
+        for output_token in data['modelargs'][mode]['maxnewtokens']:
+            lines.append(f"curl {data['envconfig']['FILEPATH']}:8088/generate -X POST -d \'{{\"inputs\":{data['envconfig']['INPUT']}, \"parameters\":{{\"max_new_tokens\":{output_token},\"do_sample\":{data['modelargs'][mode]['sample']} }} }}\' \
+                         -H \'Content-Type: application/json\' | tee -a $repopath/data/{{{data['envconfig']['INPUT']}+\"file\"}} ")
+            lines.append("sleep 2s")
+
+
+        lines.append(f"sleep 5s")
+        lines.append("")
+        runfile.writelines([line + "\n" for line in lines])
+    return generated_file
+
+
+if __name__ == '__main__':
+    if args.debug:
+        yml_file = 'bench_tgi_debug.yml'
+    data = yaml.load(open(yml_file, 'r'),Loader=yaml.FullLoader) 
+
