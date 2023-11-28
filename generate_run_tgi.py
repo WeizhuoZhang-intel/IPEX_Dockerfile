e1f21d33a9968454fed050cf6554d9ec0ebb54a4
mingxu <mingxu.zhang@intel.com>
2023-11-28 22:15:12 +0800

    debug

--- generate_run_tgi.py
+++ generate_run_tgi.py
@@ -105,9 +105,9 @@ def generate_commands(yml_file,mode):
         lines.append("do")
         if mode.endswith('bf16'):
             for output_token in data['modelargs'][mode]['maxnewtokens']:
-                filename = str(output_token) + "file"
+                filename = str(output_token) + "file.log"
                 lines.append(f"export filen={filename}")
-                lines.append(f"curl {data['envconfig']['TRUEIP']}:8088/generate -X POST -d \'{{\"inputs\":{data['envconfig']['INPUT']}, \"parameters\":{{\"max_new_tokens\":{output_token},\"do_sample\":{data['modelargs'][mode]['sample']} }} }}\' -H \'Content-Type: application/json\' | tee -a $repopath/data/$filen")
+                lines.append(f"curl {data['envconfig']['TRUEIP']}:8088/generate -X POST -d \'{{\"inputs\":{data['envconfig']['INPUT']}, \"parameters\":{{\"max_new_tokens\":{output_token},\"do_sample\":{data['modelargs'][mode]['sample'].lower()} }} }}\' -H \'Content-Type: application/json\' | tee -a $repopath/data/$filen")
                 lines.append("sleep 2s")
 
 
