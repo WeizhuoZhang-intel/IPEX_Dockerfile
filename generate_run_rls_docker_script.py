fae7e5f566c7a7d421672d9170a094a55bbda0f9
mingxu <mingxu.zhang@intel.com>
2023-12-07 11:39:41 +0800

    debug

--- generate_run_rls_docker_script.py
+++ generate_run_rls_docker_script.py
@@ -498,9 +498,9 @@ def generate_commands(yml_file,mode,extra_kmp):
                                         log_file=f"$log_dir/llm_default_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log"
                                         process_command=f"deepspeed --bind_cores_to_rank --num_accelerators {rank} --bind_core_list $core_list run.py  \
                                                             --benchmark -m {model_id} --output-dir {data['modelargs'][mode]['outputdir']} --input-tokens {input_token} --max-new-tokens {output_token} --num-iter {data['launcher']['iternum']} --dtype bfloat16 --batch-size {bs} --ipex --deployment-mode --autotp --shard-model --token-latency"
-                                        
-                                        lines.append(f"export prcpid=$(start_process ${process_command} ${log_file})")
-                                        lines.append(f"monitor_file $prcpid ${log_file}")
+                                        lines.append(f"${process_command} | tee -a ${log_file}")
+                                        # lines.append(f"export prcpid=$(start_process ${process_command} ${log_file})")
+                                        # lines.append(f"monitor_file $prcpid ${log_file}")
 
 
                                         # lines.append(f"nohup bash /root/workspace/get_mem.sh >> $log_dir/mem-usage-llm_default_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log 2>&1 || true &")
