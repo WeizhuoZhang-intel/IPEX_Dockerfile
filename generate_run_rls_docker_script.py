1dc5a4a945c4c124d12ec233c067e19fa4c0d44f
mingxu <mingxu.zhang@intel.com>
2023-12-07 13:10:58 +0800

    debug

--- generate_run_rls_docker_script.py
+++ generate_run_rls_docker_script.py
@@ -667,7 +667,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                         lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python single_instance/run_accuracy.py -m {model_id} --quantized-model-path {data['modelargs'][mode]['quantizedmodelpath']} --batch-size 56 \
                                         --dtype int8 --accuracy-only --jit --int8-bf16-mixed --tasks hellaswag \
                                         2>&1 | tee -a $log_dir/llm_accuracy_{model_id.replace('/','-')}_woq-int4_{data['launcher']['hw']}.log")                        
-                    if 'neox' in model_id:
+                    elif 'neox' in model_id:
                         lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python single_instance/run_accuracy.py -m {model_id} --quantized-model-path {data['modelargs'][mode]['quantizedmodelpath']} --batch-size 56 \
                                         --dtype int8 --accuracy-only --jit --int8 --tasks lambada_openai \
                                         2>&1 | tee -a $log_dir/llm_accuracy_{model_id.replace('/','-')}_woq-int4_{data['launcher']['hw']}.log")
