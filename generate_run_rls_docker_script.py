d81ed0cae8e63578373dfdc9ef1a23ec5ecb7377
mingxu <mingxu.zhang@intel.com>
2024-01-16 16:50:08 +0800

    deug

--- generate_run_rls_docker_script.py
+++ generate_run_rls_docker_script.py
@@ -513,7 +513,7 @@ def generate_commands(yml_file,mode,extra_kmp):
                                                                 2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
                                         else:   
                                             if 'falcon' in model_id:
-                                                lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python run.py  \
+                                                lines.append(f"python run.py  \
                                                             --benchmark -m {model_id} --ipex-weight-only-quantization --output-dir {data['modelargs'][mode]['outputdir']}/{model_id} --input-tokens {input_token} --max-new-tokens {output_token} --num-iter {data['launcher']['iternum']} --weight-dtype INT8 --quant-with-amp --batch-size {bs} --token-latency --config-file utils/model_config/tiiuae_falcon-40b_config.json \
                                                                 2>&1 | tee -a $log_dir/llm_default_{model_id.replace('/','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")  
                                             elif 'neox' in model_id:
