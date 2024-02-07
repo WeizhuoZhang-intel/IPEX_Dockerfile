df5f10e73df901217ae8aae70ce301a6e5003518
mingxu <mingxu.zhang@intel.com>
2024-02-07 13:05:47 +0800

    debug

--- generate_run_script.py
+++ generate_run_script.py
@@ -18,12 +18,66 @@ threads_per_core=$(lscpu |grep 'Thread(s) per core:' |sed 's/[^0-9]//g')
 cores_per_node=$(numactl -H |grep "node 0 cpus:" |sed 's/.*://' |awk -v tpc=$threads_per_core '{print int(NF / tpc)}')
 '''
  
+
+deepspeed_ccl_func = '''
+function deepspeed_core_config() {
+    sockets_num=$(lscpu |grep 'Socket(s):' |sed 's/[^0-9]//g')
+    cores_per_socket=$(lscpu |grep 'Core(s) per socket:' |sed 's/[^0-9]//g')
+    numa_nodes_num=$(numactl -H |grep 'node [0-9]* cpus: [0-9].*' |wc -l)
+    threads_per_core=$(lscpu |grep 'Thread(s) per core:' |sed 's/[^0-9]//g')
+    cores_per_node=$(numactl -H |grep "node 0 cpus:" |sed 's/.*://' |awk -v tpc=$threads_per_core '{print int(NF / tpc)}')
+    local_rank=$1
+    OOB_TOTAL_CORES_USE=$((${cores_per_node}*${local_rank}))
+    hbm_index=0
+    cores_per_instance=${cores_per_node}
+    numa_nodes_use_=1,$local_rank
+
+    device_array=($(numactl -H |grep "node [0-9]* cpus:" |sed "s/.*node//;s/cpus://" |sed -n "${numa_nodes_use_}p" |\\
+                    awk -v cpn=${cores_per_node} '{for(i=1;i<=cpn+1;i++) {printf(" %s ",$i)} printf("\\n");}' |grep '[0-9]' |\\
+                    awk -v cpi=${cores_per_instance} -v cpn=${cores_per_node} -v cores=${OOB_TOTAL_CORES_USE} -v hi=${hbm_index} '{
+                if(cores == "") { if(cpi > cpn) {cores = cpi}else {cores = NF} }
+                for( i=2; i<=cores; i++ ) {
+                    if($i != "") {
+                        if((i-1) % cpi == 0) {
+                            print $i";"$1+hi
+                        }else {
+                            printf $i","
+                        }
+                    }
+                }
+            }' |sed "s/,$//"))
+
+    echo ${device_array}
+    export LLM_DEEPSPEED_COMM_CORES=0
+    deepspeed_cores_list=($(echo ${device_array[@]} |sed 's/ /\\n/g' |awk -F ';' '{print $1}' |awk -F ',' -v cores=$LLM_DEEPSPEED_COMM_CORES 'BEGIN{
+                    busy = "";
+                    idle = "";
+                }{
+                    for (i=1;i<=NF;i++) {
+                        if(i==1) {
+                            idle = idle","$i;
+                            if(cores==0) {
+                                busy = busy","$i;
+                            }
+                        }else {
+                            if(i>cores) {
+                                busy = busy","$i;
+                            }
+                        }
+                    }
+                }END{
+                    printf("%s\\n%s", idle, busy);
+                }' |sed 's/^,//'))
+
+    echo $deepspeed_cores_list
+    # return $deepspeed_cores_list
+}
+'''
+
 collect_result = '''
 function collect_perf_logs_llm() {
-    ut_result=$2
-    peak_mem=$(mprof peak | grep mprofile | awk '{print $2}')
-    quant_peak_mem=${3-0}
     # latency
+    sleep 5s
     latency=($(grep -i 'inference latency:' $log_dir/$1 |sed -e 's/.*atency://;s/[^0-9.]//g;s/\.$//' |awk '
         BEGIN {
             num = 0;
@@ -99,10 +153,13 @@ function collect_perf_logs_llm() {
             }
         }
     '))
+ 
+    peak_memory=$(grep '^Total' ${log_dir}/mem-usage-$1 |sed 's/[^0-9. ]//g' |awk 'BEGIN{peak=0}{if($NF > peak){peak = $NF}}END{print peak / 1024}') || peak_memory=0
     printf $1 |tee -a ${log_dir}/summary.log
-    printf " ${latency[1]},${first_latency},${avg_latency},${p90_latency},${p99_latency},${ut_result},${peak_mem},${quant_peak_mem} \\n" |tee -a ${log_dir}/summary.log
+    printf ", ${latency[1]},${first_latency},${avg_latency},${p90_latency},${p99_latency},${peak_memory} \\n" |tee -a ${log_dir}/summary.log
 }
 '''
+
 
 def generate_commands(yml_file,mode,extra_kmp):
     data = yaml.load(open(yml_file, 'r'),Loader=yaml.FullLoader)
@@ -131,55 +188,46 @@ def generate_commands(yml_file,mode,extra_kmp):
         lines.append("mkdir -p $log_dir")
         lines.append("# device info")
         lines.append(fetch_device_info)
-        lines.append(collect_result)    
-
-        lines.append("pip install --upgrade huggingface_hub")
-        lines.append("huggingface-cli login --token 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")
+        lines.append(collect_result)   
         
         lines.append("")
         if mode.startswith('default'):
             lines.append("# Run workload")
-            lines.append("huggingface-cli login --token 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")
             for model_id in data['modelargs'][mode]['modelid']:
                 for dtype in data['modelargs'][mode]['dtype']:
                     for input_token in data['modelargs'][mode]['inputtokens']:
-                        lines.append(f"mprof clean")
-                        lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -N {data['launcher']['numactlN']} -m {data['launcher']['numactlM']} mprof run python {data['modelargs'][mode]['scriptname']} -m {model_id} --input-tokens {input_token} --dtype {dtype} --disable-deployment-mode --profile --token-latency --benchmark --num-iter 15 2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}.log")
-                        lines.append("ut_result=${PIPESTATUS[0]}")
-                        lines.append(f"collect_perf_logs_llm llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}.log $ut_result")
-                        lines.append(f"mv mprofile_*.dat $log_dir/llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}_mprofile.dat")
-        elif mode.endswith('int8'):
-            # int8 test for GPT-J and LLaMA
-            lines.append(f"mprof clean")
-            # if mode.startswith('gptj'):
-            #     lines.append("# GPT-J quantization")
-            #     lines.append(f"mprof run python run.py -m EleutherAI/gpt-j-6b --ipex-smooth-quant --batch-size 56 --calib-iters 100 --calib-shuffle --fallback-add --alpha 0.85 ")
-            # if mode.startswith('llama'):
-            #     lines.append("# LLaMA quantization")
-            #     lines.append(f"mprof run python run.py -m meta-llama/Llama-2-7b-hf --ipex-smooth-quant --lambada --output_dir {data['modelargs'][mode]['outputdir']} --jit --int8")
-            # lines.append("quant_peak_mem=$(mprof peak | grep mprofile | awk '{print $2}')")
-            # lines.append("# Run workload")
-            # lines.append(f"mprof clean")
-            # for input_token in data['modelargs'][mode]['inputtokens']:
-            #     lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -N {data['launcher']['numactlN']} -m {data['launcher']['numactlM']} mprof run python {data['modelargs'][mode]['scriptname']} --input-tokens {input_token} --benchmark --jit --int8-bf16-mixed --token-latency --num-iter 20 2>&1 | tee -a $log_dir/llm_{mode}_{input_token}.log")
-            #     lines.append("ut_result=${PIPESTATUS[0]}")
-            #     lines.append(f"collect_perf_logs_llm llm_{mode}_{input_token}.log $ut_result $quant_peak_mem")
-            #     lines.append(f"mv mprofile_*.dat $log_dir/llm_{mode}_{input_token}.dat")
+                        for output_token in data['modelargs'][mode]['maxnewtokens']:
+                            for beam in data['modelargs'][mode]['greedy']:
+                                for bs in data['modelargs'][mode]['batchsize']:
+                                    for rank in data['modelargs'][mode]['localrank']:
+                                        lines.append(f"mprof clean")
+                                        lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -N {data['launcher']['numactlN']} -m {data['launcher']['numactlM']} mprof run python {data['modelargs'][mode]['scriptname']} -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --batch-size {bs} --dtype {dtype} --disable-deployment-mode --profile --token-latency --benchmark --num-iter {data['launcher']['iternum']} 2>&1 | tee -a $log_dir/llm_default_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
+                                        lines.append("ut_result=${PIPESTATUS[0]}")
+                                        lines.append(f"collect_perf_logs_llm $log_dir/llm_default_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log $ut_result")
+                                        # lines.append(f"mv mprofile_*.dat $log_dir/llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}_mprofile.dat")
         elif mode.startswith('deepspeed'):
             lines.append("# DS Env config")
-            lines.append("huggingface-cli login --token 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")
             lines.append(f"export OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']}")
             lines.append("unset KMP_AFFINITY")
             lines.append("# Run workload")
             for model_id in data['modelargs'][mode]['modelid']:
                 for dtype in data['modelargs'][mode]['dtype']:
                     for input_token in data['modelargs'][mode]['inputtokens']:
-                        lines.append(f"mprof clean")
-                        lines.append("export FI_PROVIDER=tcp")
-                        lines.append(f"mprof run deepspeed --bind_cores_to_rank {data['modelargs'][mode]['scriptname']} --benchmark -m {model_id} --dtype {dtype} --input-tokens {input_token} --disable-deployment-mode --token-latency --num-iter 15 --profile  --autotp 2>&1 | tee -a $log_dir/llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}.log") 
-                        lines.append("ut_result=${PIPESTATUS[0]}")
-                        lines.append(f"collect_perf_logs_llm llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}.log $ut_result")
-                        lines.append(f"mv mprofile_*.dat $log_dir/llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}_mprofile.dat")
+                        for output_token in data['modelargs'][mode]['maxnewtokens']:
+                            for beam in data['modelargs'][mode]['greedy']:
+                                for bs in data['modelargs'][mode]['batchsize']:
+                                    for rank in data['modelargs'][mode]['localrank']:  
+                                        lines.append(f"mprof clean")
+                                        lines.append(f"export local_rank={rank}")
+                                        lines.append("deepspeed_core_config ${local_rank}")
+                                        lines.append("export CCL_WORKER_AFFINITY=${deepspeed_cores_list}")
+                                        lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
+
+                                        lines.append("export FI_PROVIDER=tcp")
+                                        lines.append(f"mprof run deepspeed --bind_cores_to_rank --num_accelerators {rank} --bind_core_list $core_list {data['modelargs'][mode]['scriptname']} --benchmark -m {model_id} --dtype bfloat16 -batch-size {bs} --input-tokens {input_token} --disable-deployment-mode --token-latency --num-iter {data['launcher']['iternum']} --profile  --autotp 2>&1 | tee -a $log_dir/llm_default_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log") 
+                                        lines.append("ut_result=${PIPESTATUS[0]}")
+                                        lines.append(f"collect_perf_logs_llm llm_default_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log $ut_result")
+                                        # lines.append(f"mv mprofile_*.dat $log_dir/llm_{mode}_{model_id.replace('/','_')}_{dtype}_{input_token}_mprofile.dat")
         lines.append("")
         runfile.writelines([line + "\n" for line in lines])
     return generated_file
