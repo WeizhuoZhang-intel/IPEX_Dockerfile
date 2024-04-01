7cdb50c93ba15c699e00a0d6df6a9fb666373314
mingxu <mingxu.zhang@intel.com>
2024-04-01 22:44:10 +0800

    debug

--- generate_run_rls_cluster_script.py
+++ generate_run_rls_cluster_script.py
@@ -2400,21 +2400,21 @@ def generate_commands(yml_file,mode,extra_kmp):
                         for output_token in data['modelargs'][mode]['maxnewtokens']:
                             for beam in data['modelargs'][mode]['greedy']:
                                 for bs in data['modelargs'][mode]['batchsize']:
-
-                                    lines.append(f"export local_rank={rank}")
-                                    lines.append("deepspeed_core_config ${local_rank}")
-                                    lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
-                                        
-                                    if beam == True:   
-                                        lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python /home/ubuntu/llm-modeling/run.py  \
-                                                    -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --greedy --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile    \
-                                                    2>&1 | tee -a $log_dir/llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
-                                    else:   
-                                        lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python /home/ubuntu/llm-modeling/run.py  \
-                                                    -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile \
-                                                    2>&1 | tee -a $log_dir/llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")                                            
-                            
-                                    lines.append(f"collect_perf_logs_llm llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
+                                    for rank in data['modelargs'][mode]['localrank']:
+                                        lines.append(f"export local_rank={rank}")
+                                        lines.append("deepspeed_core_config ${local_rank}")
+                                        lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
+                                            
+                                        if beam == True:   
+                                            lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python /home/ubuntu/llm-modeling/run.py  \
+                                                        -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --greedy --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile    \
+                                                        2>&1 | tee -a $log_dir/llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
+                                        else:   
+                                            lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python /home/ubuntu/llm-modeling/run.py  \
+                                                        -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile \
+                                                        2>&1 | tee -a $log_dir/llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")                                            
+                                
+                                        lines.append(f"collect_perf_logs_llm llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
 
 
 
