6aeef0b6675f810220aa187a7b8cf50dfe3bd0da
mingxu <mingxu.zhang@intel.com>
2024-04-23 11:33:43 +0800

    debug

--- generate_run_rls_cluster_script.py
+++ generate_run_rls_cluster_script.py
@@ -2448,11 +2448,11 @@ def generate_commands(yml_file,mode,extra_kmp):
                                             
                                         if beam == True:   
                                             lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python /home/ubuntu/llm-modeling/run.py  \
-                                                        -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --greedy --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile    \
+                                                        -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --greedy --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile --token-latency  \
                                                         2>&1 | tee -a $log_dir/llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
                                         else:   
                                             lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python /home/ubuntu/llm-modeling/run.py  \
-                                                        -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile \
+                                                        -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --num-iter {data['launcher']['iternum']} --batch-size {bs} --dtype {dtype}  --use-ipex-optimize --profile --token-latency \
                                                         2>&1 | tee -a $log_dir/llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")                                            
                                 
                                         lines.append(f"collect_perf_logs_llm llm_ipexllm_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
