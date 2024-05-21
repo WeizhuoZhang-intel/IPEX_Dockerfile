1eb0061385488c42bc96c35d36e64485b172063f
mingxu <mingxu.zhang@intel.com>
2024-05-21 20:23:45 +0800

    debug

--- generate_run_rls_docker_script.py
+++ generate_run_rls_docker_script.py
@@ -322,6 +322,44 @@ def generate_commands(yml_file,mode,extra_kmp):
 
 
 
+        if mode.endswith('noipex'):
+            lines.append("# Run Workload")  
+            lines.append("export WORK_DIR=./")
+            lines.append("pip uninstall -y intel-extension-for-pytorch")
+            lines.append("pip uninstall -y intel-extension-for-pytorch")
+            lines.append("pip uninstall -y transformers")
+            lines.append("pip uninstall -y transformers")
+            lines.append("cd transformers")
+            lines.append("git apply greedy.patch")
+            lines.append("python setup.py install")
+            lines.append("cd ..")
+            for model_id in data['modelargs'][mode]['modelid']:
+                for dtype in data['modelargs'][mode]['dtype']:
+                    for input_token in data['modelargs'][mode]['inputtokens']:
+                        for output_token in data['modelargs'][mode]['maxnewtokens']:
+                            for beam in data['modelargs'][mode]['greedy']:
+                                for bs in data['modelargs'][mode]['batchsize']:
+                                    for rank in data['modelargs'][mode]['localrank']:
+                                        lines.append(f"export local_rank={rank}")
+                                        lines.append("deepspeed_core_config ${local_rank}")
+                                        lines.append("export core_list=0-$(($cores_per_node*$local_rank-1))")
+
+                                        
+                                        lines.append(f"nohup bash /root/workspace/get_mem.sh >> $log_dir/mem-usage-llm_stockpt_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log 2>&1 || true &")
+                                            
+                                        if beam == True:   
+                                            lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python run.py  \
+                                                        --benchmark -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --greedy --num-iter {data['launcher']['iternum']} --dtype {dtype} --batch-size {bs} --disable-deployment-mode --token-latency --profile   \
+                                                            2>&1 | tee -a $log_dir/llm_stockpt_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
+                                        else:   
+                                            lines.append(f"OMP_NUM_THREADS={data['launcher']['OMP_NUM_THREADS']} numactl -m {data['launcher']['numactlM']} -C $core_list python run.py  \
+                                                        --benchmark -m {model_id} --input-tokens {input_token} --max-new-tokens {output_token} --greedy --num-iter {data['launcher']['iternum']} --dtype {dtype} --batch-size {bs} --disable-deployment-mode --token-latency --profile   \
+                                                            2>&1 | tee -a $log_dir/llm_stockpt_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")                                          
+                                
+                                        lines.append(f"collect_perf_logs_llm llm_stockpt_{(model_id.replace('/','-')).replace('_','-')}_{dtype}_{input_token}-{output_token}-{bs}_greedy_{beam}_NUMA_{rank}_{data['launcher']['hw']}.log")
+
+
+
         if mode.endswith('optimum'):
             lines.append("# Run Workload")  
             lines.append("export WORK_DIR=./")
